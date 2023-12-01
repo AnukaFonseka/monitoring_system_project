4fbd36746dbb2e6a1a643ae1996d829e904b4d2a
AnukaFonseka <akunafonseka@gmail.com>
2023-12-01 23:03:11 +0530

    fixed date time format in getDataValues API

--- server/service/data.service.js
+++ server/service/data.service.js
@@ -11,10 +11,26 @@ async function getDataValues() {
                 payload: "No data values found."
             }
         } else {
+            const dataValuesList = dataValues.map((data, index) => {
+                const off = data.datetime.getTimezoneOffset() * 60000;
+                var newdt = new Date(data.datetime - off).toISOString();
+                const dateAndTime = newdt.split('T');
+                const datePart = dateAndTime[0];
+                const timePart = dateAndTime[1].substring(0, 8);
+
+                return {
+                    id: data.id,
+                    temperature: data.temperature,
+                    humidity: data.humidity,
+                    date: datePart,
+                    time: timePart
+                }
+            });
+
             return {
                 error: false,
                 status: 200,
-                payload: dataValues
+                payload: dataValuesList
             };
         }
 
